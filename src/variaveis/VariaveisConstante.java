9c8f3c16edce7ea7daac1adabe3a16da623ffa41
Dilma Fagundes de Sousa Silva <kemilly.fagundes@outlook.com>
2024-10-28 13:36:31 -0300

    primeiro commit

---
+++ src/variaveis/VariaveisConstante.java
@@ -0,0 +1,24 @@
+package variaveis;
+
+public class VariaveisConstante {
+
+	public static void main(String[] args) {
+		
+		byte bit = 127;
+		short valor = 32767;
+		int quantidade = 2147483647;
+		long indentificador = 9223372036854775807L;
+		float altura = 25.45546f;
+		double area = 45.4567;
+		char tipo = 'A';
+		boolean resposta = true;
+		
+		final int QUANTIDADE = 1000;
+		
+		String palavra = "Generation Brasil";
+		
+		quantidade = 4552;
+		
+		
+	}
+}
